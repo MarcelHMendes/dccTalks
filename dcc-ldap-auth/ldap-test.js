4282aab72b5843b363ba241f344c249293aeab9d
Rafael Guedes <rafael.pardini@yahoo.com.br>
2019-09-23 20:00:41 -0300

    create a working ldap client

--- dcc-ldap-auth/ldap-test.js
+++ dcc-ldap-auth/ldap-test.js
@@ -1,24 +1,11 @@
-const ldap = require('ldapjs');
-const client = ldap.createClient({
-    url: 'ldaps://ldap.dcc.ufmg.br:389'
-});
+const LdapClient = require('ldapjs-client');
 
-//var opts = {
-//    filter: '(email=rafaelguedes@dcc.ufmg.br)'
-//};
-//
-//client.search('dc=dcc,dc=ufmg,dc=br', {}, function(err, res) {
-//    assert.ifError(err);
-//    res.on('searchEntry', function(entry) {
-//        console.log('entry: ' + JSON.stringify(entry.object));
-//    });
-//    res.on('searchReference', function(referral) {
-//        console.log('referral: ' + referral.uris.join());
-//    });
-//    res.on('error', function(err) {
-//        console.error('error: ' + err.message);
-//    });
-//    res.on('end', function(result) {
-//        console.log('status: ' + result.status);
-//    });
-//});
+const tlsOptions = {
+    requestCert: false,
+    rejectUnauthorized: false
+};
+
+const client = new LdapClient({
+    tlsOptions: tlsOptions,
+    url: 'ldaps://ldap.dcc.ufmg.br:389',
+});
