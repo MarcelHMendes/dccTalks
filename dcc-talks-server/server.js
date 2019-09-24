832ac267363fbb664528a3ed64b9a0ffed84ccb7
MarcelHMendes <marcelmendes@dcc.ufmg.br>
2019-09-24 16:50:29 -0300

    add recebimento e envio broadcast

--- dcc-talks-server/server.js
+++ dcc-talks-server/server.js
@@ -5,7 +5,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-
+/*events*/
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/public/index.html');
@@ -13,21 +13,34 @@ app.get('/', function(req, res){
 
 .use(express.static('public'));
 
+http.listen(3000, function(){
+  console.log('listening on port:3000');
+});
 
 io.on('connection', function(socket){
   console.log('a user connected');
 });
 
-http.listen(3000, function(){
-  console.log('listening on port:3000');
+io.on("connection", function(socket){
+    socket.on("chat_message_send", function(message_sent, callback){
+		message_sent = "[ " + pegarDataAtual() + " ]: " + message_sent;     
+
+         io.sockets.emit("chat_message_update", message_sent);
+         callback();
+     });
 });
 
-//io.emit('some event', { for: 'everyone' });
+/*functions*/
 
-
-io.on('connection', function(socket){
-msg='Mensagem de teste';	
-	//socket.on('chat message', function(msg){
-	    io.emit('chat message', msg);
-	//});
-});
+function pegarDataAtual(){
+  var dataAtual = new Date();
+  var dia = (dataAtual.getDate()<10 ? '0' : '') + dataAtual.getDate();
+  var mes = ((dataAtual.getMonth() + 1)<10 ? '0' : '') + (dataAtual.getMonth() + 1);
+  var ano = dataAtual.getFullYear();
+  var hora = (dataAtual.getHours()<10 ? '0' : '') + dataAtual.getHours();
+  var minuto = (dataAtual.getMinutes()<10 ? '0' : '') + dataAtual.getMinutes();
+  var segundo = (dataAtual.getSeconds()<10 ? '0' : '') + dataAtual.getSeconds();
+  
+  var dataFormatada = dia + "/" + mes + "/" + ano + " " + hora + ":" + minuto + ":" + segundo;
+  return dataFormatada;
+}
