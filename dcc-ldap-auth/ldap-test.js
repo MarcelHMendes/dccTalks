f1194b73dbe3af3519f1c28b31d43efcac6464eb
Rafael Guedes <rafael.pardini@yahoo.com.br>
2019-10-21 21:35:38 -0300

    add a working ldap test file

--- dcc-ldap-auth/ldap-test.js
+++ dcc-ldap-auth/ldap-test.js
@@ -1,11 +1,39 @@
-const LdapClient = require('ldapjs-client');
+const { Client } = require('ldapts');
 
-const tlsOptions = {
-    requestCert: false,
-    rejectUnauthorized: false
-};
+const username = '';
+const password = '';
 
-const client = new LdapClient({
-    tlsOptions: tlsOptions,
-    url: 'ldaps://ldap.dcc.ufmg.br:389',
+const url = 'ldap://ldap.dcc.ufmg.br';
+const bindDN = `uid=${username},ou=People,dc=dcc,dc=ufmg,dc=br`;
+const searchDN = 'ou=People,dc=dcc,dc=ufmg,dc=br';
+
+const client = new Client({
+    url,
+    tlsOptions: {
+        requestCert: false
+    },
 });
+
+async function auth() {
+    try {
+        await client.bind(bindDN, password);
+
+        const {
+            searchEntries,
+            searchReferences,
+        } = await client.search(searchDN, {
+            scope: 'sub',
+            filter: `(uid=${username})`,
+        });
+
+        console.log(searchEntries);
+        console.log(searchReferences);
+
+        await client.unbind();
+
+    } catch (ex) {
+        throw ex;
+    }
+}
+
+auth();
