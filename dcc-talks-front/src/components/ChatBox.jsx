6275e3d6289eb166be00a08cb19f4689d59304cd
Pedro Thomas <pedro.thomas94@gmail.com>
2019-10-23 13:38:16 -0300

    Intreface melhorada

--- dcc-talks-front/src/components/ChatBox.jsx
+++ dcc-talks-front/src/components/ChatBox.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
+import { withStyles } from '@material-ui/core/styles';
 import AppBar from "@material-ui/core/AppBar";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -16,14 +17,40 @@ import Grid from "@material-ui/core/Grid";
 
 import Input from "@material-ui/core/Input";
 
+const styles = theme => ({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  },
+});
 const useStyles = makeStyles(theme => ({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  },
   text: {
     padding: theme.spacing(2, 2, 0)
+  },
+  input:{
+    width: '100%'
   },
   paper: {
     padding: theme.spacing(2),
     textAlign: "start",
-    color: theme.palette.text.secondary
+    color: theme.palette.text.secondary,
+    flexGrow: 1,
+    overflow: "hidden",
+    padding: theme.spacing(0, 3)
   },
   list: {
     marginBottom: theme.spacing(2)
@@ -48,7 +75,8 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-export default class ChatBox extends React.Component {
+//@withStyles(styles)
+class ChatBox extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -71,7 +99,7 @@ export default class ChatBox extends React.Component {
   }
 
   render() {
-    const classes = useStyles;
+    const classes = makeStyles(styles);
     const handleChange = name => event => {
       this.setState({ ...this.state, [name]: event.target.value });
     };
@@ -89,30 +117,47 @@ export default class ChatBox extends React.Component {
 
     return (
       <React.Fragment>
-        <CssBaseline />
         <Typography className={classes.text} variant="h5" gutterBottom>
-          ChatBox
+          dccTalks
         </Typography>
         <Grid
           container
           className={classes.root}
           wrap="nowrap"
-          spacing={2}
+          width="auto"
+          text-align="left"
+          //spacing={2}
           direction="column"
           justify="flex-start"
           alignItems="flex-start"
         >
           {this.state.messages.map((message, id) => (
             <Grid item xs key={id}>
-              <Paper className={classes.paper}>
-                <div dangerouslySetInnerHTML={{ __html: message }} />
+              <Paper className={classes.paper} s={2}>
+                <Typography align="left">
+                  <div dangerouslySetInnerHTML={{ __html: message }} />
+                </Typography>
               </Paper>
             </Grid>
           ))}
         </Grid>
-        <Box bgcolor="primary.main" color="primary.contrastText" p={1} m={1}>
+        <Box
+          className={classes.root}
+          bgcolor="primary.main"
+          color="primary.contrastText"
+          width="100%"
+          position="fixed"
+          bottom={0}
+        >
           <Input
             className={classes.input}
+            autoFocus
+            rows={1}
+            rowsMax={1}
+            display='flex'
+            width="auto"
+            margin="dense"
+            multiline
             placeholder="Digite uma mensagem."
             value={this.state.text_message}
             onKeyPress={keyPressed}
@@ -130,3 +175,5 @@ export default class ChatBox extends React.Component {
     );
   }
 }
+
+export default ChatBox;
